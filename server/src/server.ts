ce05cae55b1b7af2834fe149ec5b13bebf514673
Mohan MU <mmu@netapp.com>
2025-03-06 17:14:59 +0530

    feat(server): :rocket: Generate Swagger

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import * as swaggerJson from './swagger/swagger.json';
 import { connectToDatabase } from './database';
 import { taskifyRouter } from './taskify.routes';
+import {serve,setup} from 'swagger-ui-express';
 
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
@@ -11,7 +13,8 @@ const {
     ATLAS_URI,
     SERVER_ROUTE = '/taskify',
     DB = 'taskify',
-    COLLECTION = 'tasks'
+    COLLECTION = 'tasks',
+    PORT=5200,
 } = process.env;
 
 if (!ATLAS_URI) {
@@ -25,12 +28,12 @@ connectToDatabase(ATLAS_URI, { db: DB, collection: COLLECTION })
     .then(() => {
         const app = express();
         app.use(cors());
-
         app.use(SERVER_ROUTE, taskifyRouter);
+        app.use('/docs', serve, setup(swaggerJson))
 
         // start the Express server
-        app.listen(5200, () => {
-            console.log(`Server running at http://localhost:5200...`);
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}...`);
         });
     })
     .catch((error) => console.error(error));
