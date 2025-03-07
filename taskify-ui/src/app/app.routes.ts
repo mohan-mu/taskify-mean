72fd1f7a493b7398c4a696a67417c3fb74b90639
Mohan MU <mmu@netapp.com>
2025-03-07 19:22:21 +0530

    feat(ui): :zap: Sigin and Signup Pages & Route Setup

--- taskify-ui/src/app/app.routes.ts
+++ taskify-ui/src/app/app.routes.ts
@@ -1,8 +1,32 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
+import { AUTH_KEY } from './shared/constants/constants';
+
+const canActivateTeam: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):boolean => {
+  if (localStorage.getItem(AUTH_KEY)) {
+    return true;
+  }
+
+  // not logged in so redirect to login page with the return url
+  inject(Router).navigate(['/signin'], {
+    queryParams: { returnUrl: state.url },
+  });
+  return false;
+};
 
 export const routes: Routes = [
   {
     path: 'tasks',
+    canActivate: [canActivateTeam],
     loadComponent: () =>
       import(
         './shared/components/navigation-toolbar/navigation-toolbar.component'
@@ -10,10 +34,20 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./modules/tasks/tasks.module').then(m => m.TasksModule),
   },
-
+  {
+    path: 'auth/signin',
+    loadComponent: () =>
+      import('./components/auth/signin/signin.component'),
+  },
+  {
+    path: 'auth/signup',
+    loadComponent: () =>
+      import('./components/auth/signup/signup.component'),
+  },
   {
     path: '**',
     loadComponent: () =>
       import('./shared/components/page-not-found/page-not-found.component'),
   },
 ];
+
