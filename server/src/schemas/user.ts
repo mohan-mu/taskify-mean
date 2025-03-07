b46a3ec37d57422ae3a535c3a1a73337ae569569
Mohan MU <mmu@netapp.com>
2025-03-07 18:13:23 +0530

    fix(server): :zap: Auth : Add Expiry and add email to Payload

--- server/src/schemas/user.ts
+++ server/src/schemas/user.ts
@@ -39,8 +39,11 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign(
-    { _id: user._id.toString() },
-    process.env.JWT_KEY as string
+    { _id: user._id.toString(), email: user.email },
+    process.env.JWT_KEY as string,
+    {
+      expiresIn: 86400, // 24 hours
+    }
   );
   user.tokens = user.tokens.concat({ token });
   await user.save();
